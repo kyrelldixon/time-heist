62606d7cee86c1b7d4653088b736b34b5787e31c
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-15 14:01:14 -0500

    Add Props and pass in

--- src/components/LeaderboardCard.vue
+++ src/components/LeaderboardCard.vue
@@ -4,8 +4,8 @@
       <img class="h-32 hidden md:block" src="https://via.placeholder.com/150" alt="Dream trip">
       <div class="flex flex-col justify-between ml-4">
         <div class="mb-4">
-          <p class="font-bold text-sm">Title</p>
-          <p>City</p>
+          <p class="font-bold text-sm">{{ title }}</p>
+          <p>{{ city }}</p>
         </div>
         <div class="flex items-center">
           <span class="text-xs text-gray-700 mr-2">Posted by</span>
@@ -15,7 +15,17 @@
     </div>
     <button class="font-bold flex flex-col py-4 px-8 bg-gray-100 border border-gray-300 rounded">
       <span role="img" aria-label="up arrow">🔝</span>
-      <span>23</span>
+      <span>{{ likes }}</span>
     </button>
   </article>
 </template>
+
+<script>
+export default {
+  props: [
+    "title",
+    "city",
+    "likes",
+  ]
+}
+</script>
