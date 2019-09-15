63fc707b3f8add9799b7eeb09cea367eec6cc25f
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-15 11:07:20 -0500

    Add html structure

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <template>
-  <nav class="bg-black text-white flex justify-between items-center px-8 py-4">
-    <p class="uppercase tracking-widest">Time Heist</p>
-    <div>
-      <button class="uppercase text-xs">Log In</button>
-      <button class="uppercase text-xs px-4 py-2 bg-gradient rounded ml-4">Sign Up</button>
-    </div>
-  </nav>
+  <div>
+    <nav class="bg-black text-white flex justify-between items-center px-8 py-4">
+      <p class="uppercase tracking-widest">Time Heist</p>
+      <div>
+        <button class="uppercase text-xs">Log In</button>
+        <button class="uppercase text-xs px-4 py-2 bg-gradient rounded ml-4">Sign Up</button>
+      </div>
+    </nav>
+
+    <section>
+      <h1>Leaderboard</h1>
+      <ul>
+        <li>
+          <article>
+            <img src="" alt="Dream trip">
+            <div>
+              <div>
+                <p>Title</p>
+                <p>City</p>
+              </div>
+              <div>
+                <span>Posted by</span>
+                <img src="" alt="Profile">
+              </div>
+            </div>
+          </article>
+          <button>
+            <span role="img" aria-label="up arrow">🔝</span>
+            23
+          </button>
+        </li>
+      </ul>
+    </section>
+  </div>
 </template>
 
 <style scoped>
