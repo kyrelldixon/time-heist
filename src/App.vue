f6358f7a22680e23d0be4c3170285fc2250694fb
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-15 12:09:07 -0500

    Refactor navbar and leaderboard card to components

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,23 @@
 <template>
   <div class="bg-gray-200 h-screen">
-    <nav class="bg-black text-white flex justify-between items-center px-4 md:px-8 py-4">
-      <p class="uppercase tracking-widest">Time Heist</p>
-      <div>
-        <button class="uppercase text-xs">Log In</button>
-        <button class="uppercase text-xs px-4 py-2 bg-gradient rounded ml-4">Sign Up</button>
-      </div>
-    </nav>
-
+    <navbar />
     <section>
       <div class="mx-4 lg:mx-auto mt-20 max-w-4xl">
         <h1 class="font-bold uppercase tracking-wide mb-8">Leaderboard</h1>
-          <article class="flex justify-between items-center bg-white rounded p-6 uppercase">
-            <div class="flex">
-              <img class="h-32 hidden md:block" src="https://via.placeholder.com/150" alt="Dream trip">
-              <div class="flex flex-col justify-between ml-4">
-                <div class="mb-4">
-                  <p class="font-bold text-sm">Title</p>
-                  <p>City</p>
-                </div>
-                <div class="flex items-center">
-                  <span class="text-xs text-gray-700 mr-2">Posted by</span>
-                  <img class="h-4 rounded-full" src="https://randomuser.me/api/portraits/men/86.jpg" alt="Profile">
-                </div>
-              </div>
-            </div>
-            <button class="font-bold flex flex-col py-4 px-8 bg-gray-100 border border-gray-300 rounded">
-              <span role="img" aria-label="up arrow">🔝</span>
-              <span>23</span>
-            </button>
-          </article>
+        <leaderboard-card />
       </div>
     </section>
   </div>
 </template>
 
-<style scoped>
-  .bg-gradient {
-    background: linear-gradient(to top right,#af35f1, #fa9a5e);
+<script>
+import Navbar from './components/Navbar'
+import LeaderboardCard from './components/LeaderboardCard'
+
+export default {
+  components: {
+    'navbar': Navbar,
+    'leaderboard-card': LeaderboardCard,
   }
-</style>
+}
+</script>
---
+++ src/components/LeaderboardCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <article class="flex justify-between items-center bg-white rounded p-6 uppercase">
+    <div class="flex">
+      <img class="h-32 hidden md:block" src="https://via.placeholder.com/150" alt="Dream trip">
+      <div class="flex flex-col justify-between ml-4">
+        <div class="mb-4">
+          <p class="font-bold text-sm">Title</p>
+          <p>City</p>
+        </div>
+        <div class="flex items-center">
+          <span class="text-xs text-gray-700 mr-2">Posted by</span>
+          <img class="h-12 rounded-full" src="https://randomuser.me/api/portraits/men/86.jpg" alt="Profile">
+        </div>
+      </div>
+    </div>
+    <button class="font-bold flex flex-col py-4 px-8 bg-gray-100 border border-gray-300 rounded">
+      <span role="img" aria-label="up arrow">🔝</span>
+      <span>23</span>
+    </button>
+  </article>
+</template>
