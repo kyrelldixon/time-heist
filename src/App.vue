59d1ffacd0b231f227fd7a52611e61e25cb883b8
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-15 10:55:54 -0500

    Add styled navbar

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>App.vue</h1>
+  <nav class="bg-black text-white flex justify-between items-center px-8 py-4">
+    <p class="uppercase tracking-widest">Time Heist</p>
+    <div>
+      <button class="uppercase text-xs">Log In</button>
+      <button class="uppercase text-xs px-4 py-2 bg-gradient rounded ml-4">Sign Up</button>
+    </div>
+  </nav>
 </template>
+
+<style scoped>
+  .bg-gradient {
+    background: linear-gradient(to top right,#af35f1, #fa9a5e);
+  }
+</style>
