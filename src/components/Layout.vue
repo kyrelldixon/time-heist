3d2b3c7b2926790de3da7c339bc4f1800eb7d1ed
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-19 16:42:53 -0500

    Create layout component

---
+++ src/components/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <navbar />
+    <router-view />
+  </section>
+</template>
+
+<script>
+import Navbar from '../components/Navbar'
+
+export default {
+  components: {
+    navbar: Navbar,
+  }
+}
+</script>
