afe43d0c03de37eceff489ef652fb3ec122af172
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-19 16:05:41 -0500

    Create register page and add to routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import Leaderboard from './pages/Leaderboard'
 import Landing from './pages'
+import Register from './pages/Register'
 
 const routes = [
   { path: '/', component: Landing },
   { path: '/home', component: Leaderboard },
+  { path: '/register', component: Register },
 ]
 
-export default routes;
+export default routes
