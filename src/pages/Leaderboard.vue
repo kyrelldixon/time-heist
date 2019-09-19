4fda3f80b9864d33c61bf50495171763d9a1aa64
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-19 13:24:15 -0500

    Create leaderboard and landing pages

---
+++ src/pages/Leaderboard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="bg-gray-200 h-screen">
+    <navbar />
+    <section>
+      <div class="mx-4 lg:mx-auto mt-20 max-w-4xl">
+        <h1 class="font-bold uppercase tracking-wide mb-8">Leaderboard</h1>
+        <leaderboard-card 
+          v-for="trip in sortedTrips"
+          :key="trip.id"
+          :trip="trip"
+          :upVote="upVote"
+        />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar'
+import LeaderboardCard from '../components/LeaderboardCard'
+
+export default {
+  components: {
+    'navbar': Navbar,
+    'leaderboard-card': LeaderboardCard,
+  },
+  data: function() {
+    return {
+      trips: [
+        {
+          id: 2,
+          title: "Digital Nomad Paradise",
+          city: "Chiang Mai",
+          likes: 42,
+        },
+        {
+          id: 1,
+          title: "The Best Vacation Ever",
+          city: "Paris",
+          likes: 57,
+        },
+        {
+          id: 3,
+          title: "Becoming Batman",
+          city: "Gotham",
+          likes: 23,
+        },
+      ],
+    }
+  },
+  computed: {
+    sortedTrips: function() {
+      return [...this.trips].sort((a, b) => b.likes - a.likes)
+    }
+  },
+  methods: {
+    upVote: function(id) {
+      this.trips = this.trips.map(trip => {
+        if (trip.id === id) {
+          return {
+            ...trip,
+            likes: trip.likes + 1
+          }
+        }
+        return trip;
+      });
+    }
+  }
+}
+</script>
