31475523fb4da61698290dfdaa133558e9bea3e1
Kyrell Dixon <kyrell.brown@gmail.com>
2019-09-20 16:52:21 -0500

    Dynamically display nav buttons based on route

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="bg-black text-white px-4 md:px-8 py-4">
     <div class="max-w-6xl mx-auto flex justify-between items-center">
       <router-link to="/"><img class="h-12" src="../assets/logo.png" /></router-link>
-      <div>
+      <div :class="{hidden: isHidden()}">
         <button class="uppercase text-xs">Log In</button>
         <router-link to="/register">
           <styled-button class="ml-4">Sign Up</styled-button>
@@ -18,6 +18,11 @@ import Button from '../components/Button'
 export default {
   components: {
     "styled-button": Button
+  },
+  methods: {
+    isHidden: function() {
+      return this.$route.path === '/register'
+    }
   }
 }
 </script>
