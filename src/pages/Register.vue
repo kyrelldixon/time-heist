8cdf30cf7b8102cbe6e009cfaf474bd6af1c136a
Kyrell Dixon <kyrell.brown@gmail.com>
2019-10-03 20:38:03 +0700

    Add form validation styling and messages

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -10,42 +10,56 @@
           </router-link>
         </p>
       </div>
-      <form @submit.prevent="onSubmit" class="max-w-sm" formnovalidate>
+      <form @submit.prevent="onSubmit" class="max-w-sm" novalidate>
         <div class="mb-4">
           <label class="block mb-2 text-sm" for="name">Name</label>
           <input 
             id="name"
-            class="text-gray-700 rounded w-full py-2 px-2 leading-tight focus:outline-none focus:shadow-outline"
+            class="text-gray-700 rounded w-full py-2 px-2 leading-tight border focus:outline-none focus:shadow-outline mb-1"
+            :class="{'border-red-500': $v.name.$error }"
             type="text"
             v-model.trim="$v.name.$model"
-            placeholder="Your Name">
+            placeholder="Your Name"
+          >
+          <p v-if="$v.name.$error && !$v.name.required" class="text-red-500 text-xs italic">Name is required.</p>
         </div>
         <div class="mb-4">
           <label class="block mb-2 text-sm" for="email">Email Address</label>
           <input
             id="email"
-            class="text-gray-700 rounded w-full py-2 px-2 leading-tight focus:outline-none focus:shadow-outline"
+            class="text-gray-700 rounded w-full py-2 px-2 leading-tight border focus:outline-none focus:shadow-outline mb-1"
+            :class="{'border-red-500': $v.email.$error }"
             type="email"
             v-model.trim="$v.email.$model"
-            placeholder="your@email.com">
+            placeholder="your@email.com"
+          >
+          <p v-if="$v.email.$error && !$v.email.required" class="text-red-500 text-xs italic">Email address is required.</p>
         </div>
         <div class="mb-4">
           <label class="block mb-2 text-sm" for="password">Password</label>
           <input
             id="password"
-            class="text-gray-700 rounded w-full py-2 px-2 leading-tight focus:outline-none focus:shadow-outline"
+            class="text-gray-700 rounded w-full py-2 px-2 leading-tight border focus:outline-none focus:shadow-outline mb-1"
+            :class="{'border-red-500': $v.password.$error }"
             type="password"
             v-model="$v.password.$model"
-            placeholder="Password">
+            placeholder="Password"
+          >
+          <p v-if="$v.password.$error && !$v.password.required" class="text-red-500 text-xs italic">Password is required.</p>
+          <p v-if="$v.password.$error && !$v.password.minLength" class="text-red-500 text-xs italic">Password must be at least 6 characters.</p>
         </div>
         <div class="mb-8">
           <label class="block mb-2 text-sm" for="confirm-password">Confirm Password</label>
           <input
             id="confirm-password"
-            class="text-gray-700 rounded w-full py-2 px-2 leading-tight focus:outline-none focus:shadow-outline"
+            class="text-gray-700 rounded w-full py-2 px-2 leading-tight border focus:outline-none focus:shadow-outline mb-1"
+            :class="{'border-red-500': $v.confirmPassword.$error }"
             type="password"
             v-model="$v.confirmPassword.$model"
-            placeholder="Confirm Password">
+            placeholder="Confirm Password"
+          >
+          <p v-if="$v.confirmPassword.$error && !$v.confirmPassword.required" class="text-red-500 text-xs italic">Confirm password is required.</p>
+          <p v-if="$v.confirmPassword.$error && !$v.confirmPassword.sameAs" class="text-red-500 text-xs italic">Password should be same as above.</p>
         </div>
         <styled-button>Create Account</styled-button>
       </form>
@@ -88,6 +102,7 @@ export default {
   methods: {
     onSubmit: function() {
       if (this.$v.$invalid) {
+        this.$v.$touch()
         // eslint-disable-next-line
         console.log('There ARE ERRORS')
       } else {
