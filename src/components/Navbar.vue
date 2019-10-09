5f87cf6b4b4cc269ce95c4bb55f81be9d3442670
Kyrell Dixon <kyrell.brown@gmail.com>
2019-10-09 15:50:11 +0700

    Create button leads to create page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
     <div class="max-w-6xl mx-auto flex justify-between items-center">
       <router-link to="/"><img class="h-12" src="../assets/logo.png" /></router-link>
       <div v-if="$store.state.auth.isLoggedIn" class="flex items-center" :class="{hidden: isHidden()}">
-        <span class="mr-4 text-xl cursor-pointer" role="img" aria-label="add trip">+</span>
+        <router-link to="/create" class="mr-4 text-xl cursor-pointer" role="img" aria-label="add trip">+</router-link>
         <img class="h-8 rounded-full mr-4" src="https://randomuser.me/api/portraits/men/86.jpg" alt="Profile">
         <button class="uppercase text-xs" @click="logout">Logout</button>
       </div>
