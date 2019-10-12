9d24005fd7e005c9f4d2ceaa42aca678bf9eaa8d
Kyrell Dixon <kyrell.brown@gmail.com>
2019-10-12 12:35:51 +0700

    Add loading component

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,17 +1,20 @@
 <template>
   <section class="bg-gray-200 min-h-screen">
     <navbar />
-    <p v-if="$store.state.utils.isLoading && !$store.getters.isLoggedIn">Loading...</p>
+    <!-- <loading v-if="$store.state.utils.isLoading && !$store.getters.isLoggedIn" /> -->
+    <loading v-if="true" />
     <router-view v-else/>
   </section>
 </template>
 
 <script>
 import Navbar from '../components/Navbar'
+import Loading from '../components/Loading'
 
 export default {
   components: {
     navbar: Navbar,
+    loading: Loading,
   }
 }
 </script>
---
+++ src/components/Loading.vue
@@ -0,0 +1,5 @@
+<template>
+  <section class="flex h-screen bg-black text-white items-center justify-center">
+    <p class="-mt-40 text-4xl uppercase font-bold">Loading...</p>
+  </section>
+</template>
