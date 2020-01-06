cdbc327aded179be16a7f3abe0767ccacaecc0b7
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-01-06 13:02:34 -0600

    fetch trips with loading state on create

--- src/pages/Leaderboard.vue
+++ src/pages/Leaderboard.vue
@@ -2,7 +2,9 @@
   <section class="bg-gray-200 pt-20 pb-2">
     <div class="mx-4 max-w-4xl lg:mx-auto">
       <h1 class="font-bold uppercase tracking-wide mb-8">Leaderboard</h1>
-      <leaderboard-card 
+      <loading v-if="$store.state.utils.isLoading" />
+      <leaderboard-card
+        v-else
         v-for="trip in sortedTrips"
         :key="trip.id"
         :trip="trip"
@@ -14,10 +16,15 @@
 
 <script>
 import LeaderboardCard from '../components/LeaderboardCard'
+import Loading from '../components/Loading'
 
 export default {
   components: {
     'leaderboard-card': LeaderboardCard,
+    'loading': Loading,
+  },
+  created() {
+    this.$store.dispatch('getAllTrips')
   },
   computed: {
     sortedTrips: function() {
